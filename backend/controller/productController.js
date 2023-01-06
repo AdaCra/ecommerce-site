0420ef0e45eaed78b727304b7840f8600fbed3b3
Adam Hannath <ahannath@gmail.com>
2023-01-06 15:08:27 +0100

    delete product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,59 +1,82 @@
-const Product = require('../models/products')
+const Product = require("../models/products");
 
-// create new product @ /api/v1/admin/product/new
-exports.newProduct = async(req,res,next) => {
-    const product = await Product.create(req.body)
-    res.status(201).json({
-        success: true,
-        product
-    });
-}
-
+/* ------ GENERAL ------*/
 // get all products from api/v1/products
-exports.getProducts = async (req,res,next) => {
-    const products = await Product.find();
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
 
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        message:'data to follow',
-        products
-    });
-}
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    message: "data to follow",
+    products,
+  });
+};
 
 // get single product from api/v1/products via _id
 exports.getSingleProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
-    res.status(200).json({
-        success: true,
-        product
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
     });
-}
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
 
-// get single product from api/v1/admin/products/:id
+/* ------ ADMIN ------*/
+// create new product @ /api/v1/admin/product/new
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// edit product from api/v1/admin/products/:id
 exports.updateProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
-    
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
-    }
+  let product = await Product.findById(req.params.id);
 
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
     });
+  }
 
-    res.status(200).json({
-        success: true,
-        product
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// delete product from api/v1/admin/products/:id
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
     });
-}
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Product deleted",
+  });
+};
+
+
+
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,7 +2,13 @@ const express = require('express');
 const { route } = require('../app');
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controller/productController');
+const { 
+    getProducts, 
+    newProduct, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct 
+} = require('../controller/productController');
 
 // gets
 router.route('/products').get(getProducts);
@@ -12,8 +18,8 @@ router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/new').post(newProduct);
 
 //puts
-router.route('/admin/product/:id').put(updateProduct);
-
-// delete
+router.route('/admin/product/:id')
+    .put(updateProduct)
+    .delete(deleteProduct);
 
 module.exports = router;
