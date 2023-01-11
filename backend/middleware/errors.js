777ba55cf9efaaebf5a8d845d317d43c1dbd1643
Adam Hannath <ahannath@gmail.com>
2023-01-11 20:31:59 +0100

    authentication

--- backend/middleware/errors.js
+++ backend/middleware/errors.js
@@ -2,24 +2,50 @@ const ErrorHandler = require('../utils/errorHandler.js')
 
 module.exports = (err, req, res, next) => {
     err.statusCode =  err.statusCode || 500;
+
     if(process.env.NODE_ENV !== 'DEVELOPMENT') {
         let error = {...err}
+
         error.message = err.message
-        //ID not found
+
+        // Mongoose Object ID Error
         if (err.name === 'CastError'){
             const message = `Resource not found. Invalid: ${err.path}`
             error = new ErrorHandler(message, 400)
         }
-        //Object info missing
+
+        // Mongoose Validation Error
         if (err.name === 'ValidationError'){
             const message = Object.values(err.errors).map(value => value.message);
             error = new ErrorHandler(message, 400)
         }
+
+        // Mongoose duplicate account reg errors
+        if (err.code === 11000) {
+            const message = `This ${Object.keys(err.keyValue)} already exists. Please check your ${Object.keys(err.keyValue)} or log in`
+            error = new ErrorHandler(message, 400)
+        }
+
+        // JWToken Error
+        if (err.name === 'JsonWebTokenError'){
+            const message = 'Your Web Token is invalid, Please try again';
+            error = new ErrorHandler(message, 400)
+        }
+
+        // JWToken Expired
+        if (err.name === 'TokenExpiredError'){
+            const message = 'Your Web Token has expired';
+            error = new ErrorHandler(message, 400)
+        }
     
+
+
     res.status(err.statusCode).json({
         success: false,
         message: error.message || 'Internal Server Error'
      })
+
+    //  developer mode
     } else {res.status(err.statusCode).json({
             success: false,
             error: err,
