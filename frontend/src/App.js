eeb3f211d67723defd3382a3b3719658a7419693
Adam Hannath <ahannath@gmail.com>
2023-02-19 19:48:39 +0100

    welcome back to the internet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>BigBuy</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+          <div className="container container-fluid">
+            <Routes> 
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
