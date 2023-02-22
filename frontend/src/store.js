08f65bfa5a0ee50b1c1183ff8a31a7ff3b8ccdee
Adam Hannath <ahannath@gmail.com>
2023-02-22 11:21:57 +0100

    corectedstore

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,10 +10,6 @@ const reducer = combineReducers ({
 let initialState = {};
 
 const middleware = [thunk];
-const store = createStore( 
-    reducer, 
-    initialState, 
-    composeWithDevTools, 
-    ( applyMiddleware ( ...middleware )))
+const store = createStore( reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
     export default store;
